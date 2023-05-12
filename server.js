2eb137d8b88a7cbb0504da256cdedbe3621b7575
khmdagal <khmdagal@gmail.com>
2023-05-12 12:41:45 +0100

    Add SSL auth to true

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+  }
 });
 
 app.use(cors());
