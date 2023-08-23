f72065294953512fecaeba14e1b903f6508e11b0
khmdagal <khmdagal@gmail.com>
2023-08-23 23:20:43 +0100

    Add ssl rule

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  // ssl: {
-  //   rejectUnauthorized: true,
-  // },
+  ssl: {
+    require: true,
+    rejectUnauthorized: true,
+  },
 });
 
 app.use(cors());
